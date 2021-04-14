e244509cf1d0f8f92ec6290d00f9f8039bea7328
Vasiliy_Shcherbatykh <53478860+vash050@users.noreply.github.com>
2021-04-14 19:47:29 +0300

    Auth (#2)
    * Minapp (#1)
    
    * -mainapp, static, main page
    
    * -templates, main_menu
    
    * -blocks
    
    * -auth start
    
    * -auth form, CBV
    
    * -auth form, CBV
    
    * -auth def
    
    * -SiteUser
    
    * -Task, models
    
    * -page profile
    
    * -inter
    
    * -убрал из формы регистрации профессии
    
    * -siteuser optimalisatie
    
    * -task model optimalisatie
    
    * -logout
    
    * -update
    
    * -вывод профессий
    
    * -tasks
    
    * -groups
    
    * -create task
    -task page
    
    * -create task default user
    
    * -create task default user2

--- itbegin/settings.py
+++ itbegin/settings.py
@@ -41,7 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'mainapp',
+    'authapp',
+    'groupapp',
 ]
+
+AUTH_USER_MODEL = 'authapp.SiteUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -108,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
