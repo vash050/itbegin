58186602c7fd0e7c1f3280dfd8994871b403fc12
vasiliy <vash050@rambler.ru>
2021-04-14 17:23:53 +0300

    -create task -task page

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -8,4 +8,6 @@ app_name = 'mainapp'
 urlpatterns = [
     path('', mainapp.index, name='index'),
     path('tasks/', mainapp.tasks, name='tasks'),
+    path('task/<int:pk>', mainapp.task, name='task'),
+    path('create_task/', mainapp.create_task, name='create_task'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
 
+from mainapp.forms import CreateTaskForm
 from mainapp.models import Task
 
 
@@ -15,3 +17,31 @@ def tasks(request):
 
     content = {'title': title, 'tasks': tasks}
     return render(request, 'mainapp/tasks.html', context=content)
+
+
+def task(request, pk):
+    title = 'задача'
+    task_obj = Task.objects.get(pk=pk)
+
+    content = {'title': title, 'task': task_obj}
+    return render(request, 'mainapp/task.html', context=content)
+
+def create_task(request):
+    title = 'создание задачи'
+
+    if request.method == "POST":
+        create_task_form = CreateTaskForm(request.POST, request.FILES)
+
+        if create_task_form.is_valid():
+            create_task_form.save(commit=False)
+            create_task_form.author = f'{request.user}'
+            create_task_form.save()
+
+            return HttpResponseRedirect(reverse('mainapp:tasks'))
+
+    else:
+        create_task_form = CreateTaskForm()
+
+    content = {'title': title, 'create_task_form': create_task_form}
+
+    return render(request, "mainapp/create_task.html", context=content)
