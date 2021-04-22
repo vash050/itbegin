74c9052fb02a4b683c2a58c7e5daa06a2ef13a6b
Vasiliy <vash050@rambler.ru>
2021-04-22 13:30:59 +0300

    -messageapp start

---
+++ messegeapp/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+
+import messegeapp.views as messegeapp
+
+app_name = 'messegeapp'
+
+urlpatterns = [
+#     path('messegeapp/', messegeapp.groups, name='groups'),
+ ]
