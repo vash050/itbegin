f94013bc66dfec99475878b048ec5e6ec581d3b6
Vasiliy_Shcherbatykh <53478860+vash050@users.noreply.github.com>
2021-05-31 16:39:03 +0300

    Groups (#9)
    * -profile p+
    
    * -_create_group
    
    * -group - free vacancy
    
    * -group prof+
    
    * -fix s
    
    * -group prof++
    
    * -1111
    
    * -1111+

--- groupapp/forms.py
+++ groupapp/forms.py
@@ -1,10 +1,26 @@
 from django.forms.models import ModelForm
+from django.forms.widgets import HiddenInput
 
-from groupapp.models import Group
+from groupapp.models import Group, Professions, DescriptionNeedProfessions
 
 
-class CreateGroup(ModelForm):
+class CreateGroupForm(ModelForm):
     class Meta:
         model = Group
-        fields = ('name', 'description', 'need_profession','logotype')
+        fields = ('name', 'description', 'need_profession', 'logotype')
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs["class"] = "input_type_black registration__form_input"
+
+
+class UpdateVacancyForm(ModelForm):
+    class Meta:
+        model = DescriptionNeedProfessions
+        fields = ['id', 'group', 'profession', 'description']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs["class"] = "input_type_black registration__form_input"
--- groupapp/urls.py
+++ groupapp/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('groups/', groupapp.groups, name='groups'),
     path('group/<int:pk>', groupapp.group, name='group'),
     path('create_group/', groupapp.create_group, name='create_group'),
+    path('create_request_in_team/', groupapp.create_request_in_team, name='create_request_in_team'),
+    path('setting/<int:pk>/', groupapp.SettingView.as_view(), name='setting_group'),
+    path('update_vacancy/<int:pk>/', groupapp.VacancyUpdate.as_view(), name='update_vacancy'),
+
 ]
