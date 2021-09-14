923e0afe1906094bf5f6213c5633c9f91ab62d76
Vasiliy <vash050@rambler.ru>
2021-09-14 15:10:15 +0300

    -fix create_task.html

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -11,4 +11,4 @@ class CreateTaskForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(CreateTaskForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
-            field.widget.attrs["class"] = "form-control"
+            field.widget.attrs["class"] = "input_type_black registration__form_input"
