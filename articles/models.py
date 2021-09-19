f75e7770ad4e7da18e00417ca7f70d857efcab75
Vasiliy <vash050@rambler.ru>
2021-09-19 17:07:38 +0300

    - картинки статей

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,8 @@
+from django.db import models
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.core.fields import RichTextField
 from wagtail.core.models import Page
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 from authapp.models import SiteUser
 
@@ -25,12 +27,20 @@ class Article(Page):
     model article
     """
     subpage_types = []
+    image = models.ForeignKey('wagtailimages.Image', null=True, on_delete=models.SET_NULL, related_name='+')
     body = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
+        ImageChooserPanel('image'),
         FieldPanel('owner', classname="full"),
+
     ]
 
     settings_panels = []
     promote_panels = []
+
+    def avatar_or_default(self, default_path="../static/img/news1.png"):
+        if self.image:
+            return self.image
+        return default_path
