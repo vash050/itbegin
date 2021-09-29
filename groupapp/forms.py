8d772f95e010015369eb2a75d249abdce3f2d540
TamaraTceveen <toma8612@gmail.com>
2021-09-29 12:13:54 +0200

    styled forms, groupapp, taskapp

--- groupapp/forms.py
+++ groupapp/forms.py
@@ -17,6 +17,8 @@ class CreateGroupForm(ModelForm):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs["class"] = "input_type_black registration__form_input"
+            if field_name == 'need_profession':
+                field.widget.attrs["class"] = "input_type_checkbox"
 
 
 class UpdateVacancyForm(ModelForm):
