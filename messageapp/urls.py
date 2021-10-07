6ed566739925cdee599b985a61f38050392ad175
Vasiliy <vash050@rambler.ru>
2021-10-07 11:44:21 +0300

    - check message

--- messageapp/urls.py
+++ messageapp/urls.py
@@ -7,5 +7,6 @@ app_name = 'messageapp'
 urlpatterns = [
     path('dialogs/', messageapp.DialogsView.as_view(), name='dialog'),
     path('dialogs/creare/<int:user_id>/', messageapp.CreateDialogView.as_view(), name='create_dialog'),
-    path('dialogs/<int:dialog_id>/', messageapp.MessagesView.as_view(), name='messages'),
+    # path('dialogs/<int:dialog_id>/', messageapp.MessagesView.as_view(), name='messages'),
+    path('dialogs/<int:dialog_id>/', messageapp.DialogsView.as_view(), name='dialog'),
 ]
