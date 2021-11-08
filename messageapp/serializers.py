76c94f80743142800bfe03a66ba10c2e42d991e8
Vasiliy <vash050@rambler.ru>
2021-11-08 16:54:06 +0300

    - json + данные об участниках чата

--- messageapp/serializers.py
+++ messageapp/serializers.py
@@ -4,6 +4,7 @@ from rest_framework import serializers
 from rest_framework.relations import PrimaryKeyRelatedField
 from rest_framework.serializers import ModelSerializer
 
+from authapp.models import SiteUser
 from messageapp.models import Dialog, Message
 
 
@@ -13,10 +14,18 @@ class DialogSerializer(ModelSerializer):
         fields = ['members', 'last_message', 'objects']
 
 
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = SiteUser
+        fields = ['first_name', 'last_name', 'id', 'username']  # оставить только username
+
+
 class MessageSerializer(ModelSerializer):
     user_name = serializers.CharField(source="author.username", read_only=True)
     user_avatar = serializers.ImageField(source="author.avatar", read_only=True)
+    all_members = UserSerializer(source="dialog.members", many=True, read_only=True)
 
     class Meta:
         model = Message
-        fields = ['dialog', 'author', 'user_name', 'user_avatar', 'message', 'pub_date', 'is_read', 'is_active']
+        fields = ['dialog', 'all_members', 'author', 'user_name', 'user_avatar', 'message', 'pub_date', 'is_read',
+                  'is_active']
