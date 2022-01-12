f4e59166174a51c49cd15fe9aeeffe77b702306c
TamaraTceveen <68911827+TamaraTceveen@users.noreply.github.com>
2022-01-12 17:48:01 +0100

    Team pag (#35)
    * pages tasks, task, about
    
    * ...
    
    * some little changes
    
    * /.
    
    * ...
    
    * ..
    
    * prof list changes

--- static/js/script_custom_select.js
+++ static/js/script_custom_select.js
@@ -1,94 +1,27 @@
 "use strict";
+let checkboxes, checkedDiv, enabledSettings, opacity;
 
-var l, newDivOption;
+if (document.querySelectorAll("input[type=checkbox][name=profession]")) {
+  checkboxes = document.querySelectorAll(
+    "input[type=checkbox][name=profession]"
+  );
+  checkedDiv = document.getElementsByClassName("checkmark");
 
-/* Look for any elements with the class "custom-select": */
-let block = document.getElementsByClassName("registration__form__block");
-
-for (let i = 0; i < block.length; i++) {
-  let selElmnt = block[i].getElementsByTagName("select")[0];
-
-  /* For each element, create a new DIV that will act as the selected item: */
-  let newDivSelected = document.createElement("DIV");
-  newDivSelected.setAttribute("class", "select-selected");
-  newDivSelected.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-  block[i].appendChild(newDivSelected);
-
-  /* For each element, create a new DIV that will contain the option list: */
-  let newDivHide = document.createElement("DIV");
-  newDivHide.setAttribute("class", "select-items select-hide");
-
-  for (let j = 1; j < selElmnt.length; j++) {
-    /* For each option in the original select element,
-        create a new DIV that will act as an option item: */
-    newDivOption = document.createElement("DIV");
-    newDivOption.innerHTML = selElmnt.options[j].innerHTML;
-
-    newDivOption.addEventListener("click", function (e) {
-      /* When an item is clicked, update the original select box,
-            and the selected item: */
-      var y, s, h;
-      s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-
-      h = this.parentNode.previousSibling;
-      for (let i = 0; i < s.length; i++) {
-        if (s.options[i].innerHTML == this.innerHTML) {
-          s.selectedIndex = i;
-          h.innerHTML = this.innerHTML;
-          y = this.parentNode.getElementsByClassName("same-as-selected");
-
-          for (let k = 0; k < y.length; k++) {
-            y[k].removeAttribute("class");
-          }
-          this.setAttribute("class", "same-as-selected");
-          break;
-        }
+  checkboxes.forEach(function (checkbox) {
+    if (checkbox.checked) {
+      changeOpacity(checkedDiv[checkbox.value - 1], "1");
+    }
+    checkbox.addEventListener("change", function () {
+      if (checkbox.checked) {
+        opacity = "1";
+      } else {
+        opacity = "0";
       }
-      h.click();
+      changeOpacity(checkedDiv[checkbox.value - 1], opacity);
     });
-    newDivHide.appendChild(newDivOption);
-  }
-
-  block[i].appendChild(newDivHide);
-  newDivSelected.addEventListener("click", function (e) {
-    /* When the select box is clicked, close any other select boxes,
-        and open/close the current select box: */
-    e.stopPropagation();
-    closeAllSelect(this);
-    this.nextSibling.classList.toggle("select-hide");
-    this.classList.toggle("select-arrow-active");
   });
 }
-function closeAllSelect(elmnt) {
-  /* A function that will close all select boxes in the document,
-    except the current select box: */
-  var x,
-    y,
-    arrNo = [];
-  x = document.getElementsByClassName("select-items");
-  y = document.getElementsByClassName("select-selected");
 
-  for (let i = 0; i < y.length; i++) {
-    if (elmnt == y[i]) {
-      arrNo.push(i);
-    } else {
-      y[i].classList.remove("select-arrow-active");
-    }
-  }
-  for (let i = 0; i < x.length; i++) {
-    if (arrNo.indexOf(i)) {
-      x[i].classList.add("select-hide");
-    }
-  }
-  console.log("close all clicked");
+function changeOpacity(block, data) {
+  block.style.opacity = data;
 }
-
-/* If the user clicks anywhere outside the select box,
-then close all select boxes: */
-document.body.addEventListener("click", function (event) {
-  console.log("window clicked");
-  if (event.target != block) {
-    console.log("if clicked");
-    closeAllSelect();
-  }
-});
